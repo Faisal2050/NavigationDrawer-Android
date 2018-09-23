5d38a026fd667a278143537f00297fbfa1d3bddc
Faisal Shahzad <40182731+Faisal2050@users.noreply.github.com>
2018-09-23 23:58:30 +0500

    first commit

---
+++ app/src/main/java/com/example/faisal/navigationdrawer/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.faisal.navigationdrawer;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerLayout=(DrawerLayout) findViewById(R.id.activity_main);
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+            return super.onOptionsItemSelected(item);
+    }
+}
